0f1f8c5033f797cc64e8ab9dbe50af36dbe32a5a
Ultradev <38569555+ultranatum@users.noreply.github.com>
2019-03-29 22:04:07 +0300

    Update ulg_install.sh

--- ulg_install.sh
+++ ulg_install.sh
@@ -8,7 +8,7 @@ COIN_CLI='ultragate-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/ultranatum/ultragate/releases/download/v1.0.0.2/ultragate-1.0.0.2-x86_64-linux-gnu.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='UltaGate'
+COIN_NAME='Ultagate'
 COIN_PORT=32852
 RPC_PORT=32853
 
